1582d1bfbcf558122524ca58c4d311aabc268178
Jose Rivillas <josearivillas64@gmail.com>
2024-09-15 16:08:42 -0500

    feat(hu8): add spring security + jwt validation

--- stock-service/src/main/java/com/emazon/stock_service/infraestructure/exceptionhandler/ExceptionResponse.java
+++ stock-service/src/main/java/com/emazon/stock_service/infraestructure/exceptionhandler/ExceptionResponse.java
@@ -6,7 +6,8 @@ public enum ExceptionResponse {
     CATEGORY_NOT_FOUND_BY_ID("No se encontró la categoría con ese id"),
     CATEGORY_ALREADY_EXISTS("Ya existe una categoría con ese nombre"),
     BRAND_NOT_FOUND_BY_ID("No se encontró la marca con ese id"),
-    PRODUCTS_NOT_FOUND("No se encontraron productos");
+    PRODUCTS_NOT_FOUND("No se encontraron productos"),
+    PRODUCT_ALREADY_EXISTS("Ya existe un producto con ese nombre"),;
 
 
     private String message;
